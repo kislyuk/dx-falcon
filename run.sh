585afa6b3c4fd9550afbd0d7f1fd83b324744e41
Andrey Kislyuk <kislyuk@gmail.com>
2015-01-14 23:29:55 -0800

    WIP

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # E. coli
 #dx build -f --run $(for i in $(dx ls "Falcon Test:/data/*.fasta" --brief); do echo "-i reads=$i"; done) --debug-on AppInternalError --yes --watch
 
 # A. thaliana
-echo dx build -f --run -i config_file="Falcon Test:/fc_run_arab.cfg" $(for i in $(dx ls "Falcon Test:/data/a.thaliana/*.fasta" --brief); do echo "-i reads=$i"; done) --debug-on AppInternalError --yes --watch
+dx build -f --run -i config_file="Falcon Test:/fc_run_arab.cfg" $(for i in $(dx ls "Falcon Test:/data/a.thaliana/*.fasta" --brief); do echo "-i reads=$i"; done) --debug-on AppInternalError --yes --watch
