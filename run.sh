c88fe5f713b1a8d34deda9b2dd77917449ac4e53
Andrey Kislyuk <kislyuk@gmail.com>
2015-01-19 17:20:09 -0800

    WIP

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@
 #dx build -f --run $(for i in $(dx ls "Falcon Test:/data/*.fasta" --brief); do echo "-i reads=$i"; done) --debug-on AppInternalError --yes --watch
 
 # A. thaliana
-dx build -f --run -i config_file="Falcon Test:/fc_run_arab.cfg" $(for i in $(dx ls "Falcon Test:/data/a.thaliana/*.fasta" --brief); do echo "-i reads=$i"; done) --debug-on AppInternalError --yes --watch
+dx build -f --run -i config_file="Falcon Test:/fc_run_arab.cfg" $(for i in $(dx ls "Falcon Test:/data/a.thaliana/*.fasta" --brief); do echo "-i reads=$i"; done) --yes --watch --allow-ssh #--debug-on AppInternalError
