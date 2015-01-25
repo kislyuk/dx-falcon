8b9ea7e996ec733fe301e4905548da593a07ec1d
Andrey Kislyuk <kislyuk@gmail.com>
2015-01-25 04:53:03 -0800

    WIP

--- src/falcon.py
+++ src/falcon.py
@@ -9,7 +9,11 @@ subprocess.check_call(["chmod", "a+rwx", "falcon"])
 os.umask(0)
 
 @dxpy.entry_point('main')
-def main(reads, length_cutoff, preassembly_length_cutoff, config_file=None):
+def main(reads, length_cutoff, preassembly_length_cutoff, config_file=None, script_file=None, fs_key=None, origin_job_addr=None):
+    # Hack: the app runs itself since subjob restartability is not available.
+    if script_file:
+        return run_script(script_file, fs_key, origin_job_addr)
+
     subprocess.check_call(["ssh-keygen", "-t", "rsa", "-N", "", "-f", "id_rsa"])
     with open("id_rsa.pub") as pubkey, open(".ssh/authorized_keys", "ab") as authorized_keys:
         authorized_keys.write(pubkey.read())
@@ -23,26 +27,28 @@ def main(reads, length_cutoff, preassembly_length_cutoff, config_file=None):
             dxpy.download_dxfile(f.get_id(), os.path.join(wd, f.name))
             fofn.write(os.path.join(wd, f.name) + "\n")
 
+    shutil.copy("falcon.cfg", wd)
     if config_file:
         dxpy.download_dxfile(dxpy.DXFile(config_file).get_id(), os.path.join(wd, "falcon.cfg"))
 
-    shutil.copy("falcon.cfg", wd)
     subprocess.check_call(["fc_run.py", "falcon.cfg"], cwd=wd)
 
     assembly = dxpy.upload_local_file(os.path.join(wd, "2-asm-falcon/p_ctg.fa"))
 
     output = {}
     output["assembly"] = dxpy.dxlink(assembly)
-
     return output
 
-@dxpy.entry_point('run_script')
-def run_script(script_file, origin_job_addr):
-    dxpy.download_dxfile(dxpy.search.find_one_data_object(name="id_rsa")["id"], "id_rsa")
+#@dxpy.entry_point('run_script')
+def run_script(script_file, fs_key, origin_job_addr):
+    dxpy.download_dxfile(dxpy.DXFile(fs_key).get_id(), "id_rsa")
     subprocess.check_call(["chmod", "go-rwx", "id_rsa"])
     dxpy.download_dxfile(dxpy.DXFile(script_file).get_id(), "run.sh")
     time.sleep(1)
     subprocess.check_call(["sshfs", "-o", "StrictHostKeyChecking=no", "-o", "IdentityFile=/home/dnanexus/id_rsa", "dnanexus@{}:{}".format(origin_job_addr, wd), wd])
     subprocess.check_call("set -e; source ../run.sh", shell=True, executable="/bin/bash", cwd=wd)
+
+    # Hack: return a dummy value when running a subjob as an app, to satisfy output spec
+    return {"assembly": script_file}
 
 dxpy.run()
